2efb0408da7fd64fe2c07a53c85b0b37c70816d1
Nitish Gupta <nigupta@nigupta-a01.vmware.com>
2020-08-08 10:34:37 +0530

    f

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import Game from "./scenes/game"
 import io from 'socket.io-client';
 
 //let socket = io('http://35.244.33.191:3000');
-let socket = io('http://10.160.0.5');
+let socket = io('http://10.160.0.5:3000');
 //let socket = io('http://localhost:3000');
 
 const config = {
